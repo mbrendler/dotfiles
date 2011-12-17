6773936eb44dac9a04a90373a13e180c9f74ada9
Maik Brendler <sellerieschnitzel@googlemail.com>
2011-12-17 16:59:41 +0100

    Move configuration files to 'configfiles' directory.

--- createlinks.py
+++ createlinks.py
@@ -2,19 +2,19 @@
 import os
 
 
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.dirname(__file__)
+SOURCE_DIR = os.path.abspath(os.path.join(HERE, 'configfiles'))
 THIS_SCRIPT = os.path.basename(__file__)
 DESTINATION_DIR = os.getenv('HOME')
 
 
 def main():
-    for filename in os.listdir(HERE):
-        if THIS_SCRIPT != filename and not filename.startswith('.'):
-            source_path = os.path.join(HERE, filename)
-            destination_path = os.path.join(DESTINATION_DIR, '.' + filename)
-            link_command = '/bin/ln -s %s %s' % (source_path, destination_path)
-            print(link_command)
-            os.system(link_command)
+    for filename in os.listdir(SOURCE_DIR):
+        source_path = os.path.join(SOURCE_DIR, filename)
+        destination_path = os.path.join(DESTINATION_DIR, '.' + filename)
+        link_command = '/bin/ln -s %s %s' % (source_path, destination_path)
+        print(link_command)
+        os.system(link_command)
 
 
 if __name__ == '__main__':
