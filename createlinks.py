2ac3415f6f4144d20b31275bfc6b96ce7e981352
Maik Brendler <sellerieschnitzel@googlemail.com>
2011-12-17 17:15:59 +0100

    Colorize error messages.

--- createlinks.py
+++ createlinks.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import os
+import sys
 
 
 HERE = os.path.dirname(__file__)
 SOURCE_DIR = os.path.abspath(os.path.join(HERE, 'configfiles'))
 THIS_SCRIPT = os.path.basename(__file__)
 DESTINATION_DIR = os.getenv('HOME')
+
+
+def error(message):
+    sys.stderr.write("\033[38;5;1m" + str(message) + "\033[0m\n")
+    sys.stderr.flush()
 
 
 def main():
@@ -17,7 +23,7 @@ def main():
         try:
             os.symlink(source_path, destination_path)
         except OSError, e:
-            print(e)
+            error(e)
 
 
 if __name__ == '__main__':
