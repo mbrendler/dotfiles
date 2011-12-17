7bf5791316c0bfb1ea379d79dfe41c493347b0a7
Maik Brendler <sellerieschnitzel@googlemail.com>
2011-12-17 17:30:22 +0100

    Processed pylint messages.

--- createlinks.py
+++ createlinks.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
+"""Creates symbolic links for every file and directory from 'configfiles'.
+
+The links are created in home directory of the current user. Every link gets a
+'.' prefix.
+"""
 import os
 import sys
 
 
 HERE = os.path.dirname(__file__)
 SOURCE_DIR = os.path.abspath(os.path.join(HERE, 'configfiles'))
-THIS_SCRIPT = os.path.basename(__file__)
 DESTINATION_DIR = os.getenv('HOME')
 
 
 def error(message):
+    """Prints error messages to stderr in red.
+    """
     sys.stderr.write("\033[38;5;1m" + str(message) + "\033[0m\n")
     sys.stderr.flush()
 
 
 def main():
+    """See module documentation.
+    """
     for filename in os.listdir(SOURCE_DIR):
         source_path = os.path.join(SOURCE_DIR, filename)
         destination_path = os.path.join(DESTINATION_DIR, '.' + filename)
-        link_command = '/bin/ln -s %s %s' % (source_path, destination_path)
         print('%s -> %s' % (source_path, destination_path))
         try:
             os.symlink(source_path, destination_path)
-        except OSError, e:
-            error(e)
+        except OSError, err:
+            error(err)
 
 
 if __name__ == '__main__':
