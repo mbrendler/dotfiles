40f2374f0ab3c8a3cb76ce42cb0a74bfaa62c205
Maik Brendler <sellerieschnitzel@googlemail.com>
2015-09-14 21:26:24 +0200

    Install/update hg crecord with update.sh

--- update.sh
+++ update.sh
@@ -19,3 +19,11 @@ popd
 pip install -U pip
 pip install -U mercurial-keyring
 pip install -U hg-git
+
+LOCAL_PYTHON_PACKAGES="$HOME/.python_packages"
+mkdir -p $LOCAL_PYTHON_PACKAGES
+if test -e "$LOCAL_PYTHON_PACKAGES/crecord" ; then
+  hg -R "$LOCAL_PYTHON_PACKAGES/crecord" pull -u
+else
+  hg clone https://bitbucket.org/edgimar/crecord "$LOCAL_PYTHON_PACKAGES/crecord"
+fi
