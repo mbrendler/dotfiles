d614780a9caa6a8c24e06d149c8a4185eb0d655f
Maik Brendler <sellerieschnitzel@googlemail.com>
2015-09-14 21:29:12 +0200

    Do not install vim-powerline fonts via update.sh
    I don't use vim-powerline anymore.

--- update.sh
+++ update.sh
@@ -4,14 +4,6 @@ set -ex
 
 vim +BundleInstall! +qall
 
-if test "$(uname)" == 'Darwin' ; then
-    mkdir -p "$HOME/Library/Fonts/"
-    cp extras/Monaco-Powerline.otf "$HOME/Library/Fonts/"
-elif test "$(uname)" == 'Linux' ; then
-    mkdir -p "$HOME/.fonts/"
-    cp extras/Monospace-Powerline.ttf "$HOME/.fonts/"
-fi
-
 pushd ~/.vim/bundle/jedi-vim/
 git submodule update --init
 popd
