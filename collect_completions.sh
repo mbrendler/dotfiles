31d1a681d48132a3de7dc57e165a44d444fd51d3
Maik Brendler <sellerieschnitzel@googlemail.com>
2015-10-18 22:28:21 +0200

    Update completion collector

--- collect_completions.sh
+++ collect_completions.sh
@@ -2,23 +2,23 @@
 
 
 COMPLETION_DEFINITIONS=(
-  "bower:$HOME/.zsh/zsh-completions/src/_bower"
+  "bower:$HOME/.usr/plugins/zsh/zsh-completions/src/_bower"
   "brew:/usr/local/Library/Contributions/brew_zsh_completion.zsh"
-  "bundler:$HOME/.zsh/zsh-completions/src/_bundle"
-  "cabal:$HOME/.zsh/zsh-completions/src/_cabal"
-  "cmake:$HOME/.zsh/zsh-completions/src/_cmake"
-  "gem:$HOME/.zsh/zsh-completions/src/_gem"
-  "ghc:$HOME/.zsh/zsh-completions/src/_ghc"
-  "hg:/usr/local/opt/mercurial/share/zsh/site-functions/_hg"
+  "bundler:$HOME/.usr/plugins/zsh/zsh-completions/src/_bundle"
+  "cabal:$HOME/.usr/plugins/zsh/zsh-completions/src/_cabal"
+  "cmake:$HOME/.usr/plugins/zsh/zsh-completions/src/_cmake"
+  "gem:$HOME/.usr/plugins/zsh/zsh-completions/src/_gem"
+  "ghc:$HOME/.usr/plugins/zsh/zsh-completions/src/_ghc"
+  "hg:$HOME/usr/local/opt/mercurial/share/zsh/site-functions/_hg"
   "i:$HOME/Documents/src/i/zsh-completion/_i"
-  "jq:$HOME/.zsh/zsh-completions/src/_jq"
-  "node:$HOME/.zsh/zsh-completions/src/_node"
+  "jq:$HOME/.usr/plugins/zsh/zsh-completions/src/_jq"
+  "node:$HOME/.usr/plugins/zsh/zsh-completions/src/_node"
   "npm:/usr/local/etc/bash_completion.d/npm"
-  "pip:$HOME/.zsh/oh-my-zsh/plugins/pip/_pip"
+  "pip:$HOME/.usr/plugins/zsh/oh-my-zsh/plugins/pip/_pip"
   "tig:/usr/local/etc/bash_completion.d/tig-completion.bash"
   "tmux:/usr/local/etc/bash_completion.d/tmux"
-  "vagrant:$HOME/.zsh/zsh-completions/src/_vagrant"
-  "watch:$HOME/.zsh/zsh-completions/src/_watch"
+  "vagrant:$HOME/.usr/plugins/zsh/zsh-completions/src/_vagrant"
+  "watch:$HOME/.usr/plugins/zsh/zsh-completions/src/_watch"
 )
 
 
