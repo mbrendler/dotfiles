0a67ebecab9296b8b51e9648326f15970a9873df
Maik Brendler <sellerieschnitzel@googlemail.com>
2016-05-20 18:10:12 +0200

    Fix style issues in menu.py

--- dot/bin/menu.py
+++ dot/bin/menu.py
@@ -1,10 +1,12 @@
+# pylint: disable=invalid-name,missing-docstring
 from curses import (
-    wrapper, A_REVERSE, A_NORMAL, KEY_ENTER, KEY_RESIZE,
+    wrapper, KEY_ENTER, KEY_RESIZE,
     use_default_colors, color_pair, init_pair, COLOR_GREEN
 )
 import locale
 
 locale.setlocale(locale.LC_ALL, '')
+
 
 class Menu(object):
     def __init__(self, selected=0, scroll_offset=0, list_top=0, list_bottom=0):
@@ -23,7 +25,7 @@ class Menu(object):
             27: lambda *_: False
         }
 
-    def meta_key(self, window, _key_code):
+    def meta_key(self, window, _):
         key_code = window.getch()
         action = self._meta_key_actions.get(key_code, lambda *_: None)
         return action(window, key_code)
@@ -49,9 +51,10 @@ class Menu(object):
 
     def select_next(self, window, key_code):
         self._selected = min(self._selected + 1, self.entries_count() - 1)
-        if self._selected == 0:
+        old_pos = self._top + self.list_height(window) - self._scroll_offset
+        if self._selected <= 0:
             self.select_first(window, key_code)
-        elif self._selected >= self._top + self.list_height(window) - self._scroll_offset:
+        elif self._selected >= old_pos:
             last_top = self.entries_count() - self.list_height(window)
             self._top = min(last_top, self._top + 1)
 
@@ -79,7 +82,8 @@ class Menu(object):
         list_height = height - self._list_top - self._list_bottom
         entries_to_display = self.entries()[self._top:self._top + list_height]
         for i, entry in enumerate(entries_to_display, start=self._list_top):
-            window.addstr(i, 0, self.displayed_text_by_entry(entry)[:width - 2])
+            text = self.displayed_text_by_entry(entry)[:width - 2]
+            window.addstr(i, 0, text)
             window.clrtobot()
             is_selected = self._selected == self._top + i - self._list_top
             window.chgat(i, 0, color_pair(2) if is_selected else color_pair(1))
