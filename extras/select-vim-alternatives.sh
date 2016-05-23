54dd4558cd5184b26f50e43e9a5366e773e75821
Maik Brendler <sellerieschnitzel@googlemail.com>
2016-05-23 14:17:10 +0200

    Add nvim-support in select-vim-alternatives

--- extras/select-vim-alternatives.sh
+++ extras/select-vim-alternatives.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
 
@@ -6,17 +6,17 @@ PANES=$(tmux list-panes -F "#{pane_active},#{pane_id},#{pane_current_command}")
 
 CURRENT_COMMAND="$(echo "$PANES" | grep '^1' | cut -d, -f3)"
 
-if test "vim" != "$CURRENT_COMMAND" ; then
-  TARGET_PANE="$(echo "$PANES" | grep ',vim$' | head -n1 | cut -d, -f2)"
-  if test -z "$TARGET_PANE" ; then
-    TARGET_PANE=":.+"
-  fi
-  tmux select-pane -t "$TARGET_PANE"
-else
+if [ vim = "$CURRENT_COMMAND" ] || [ nvim = "$CURRENT_COMMAND" ] ; then
   if test "pane-switch" == "$1" ; then
     # C-7
     tmux send-keys 
   fi
+else
+  TARGET_PANE="$(echo "$PANES" | grep ',vim$' | head -n1 | cut -d, -f2)"
+  if [ -z "$TARGET_PANE" ] ; then
+    TARGET_PANE=":.+"
+  fi
+  tmux select-pane -t "$TARGET_PANE"
 fi
 
 if test "alternative" == "$1" ; then
