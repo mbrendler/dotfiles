07fbacee45dc864f07c0ae47aacae68a5d7adcb3
Maik Brendler <maik.brendler@injixo.com>
2018-05-25 22:41:59 +0200

    Fix ZSH completion for git and tig

--- collect-completions.sh
+++ collect-completions.sh
@@ -11,6 +11,7 @@ COMPLETION_DEFINITIONS=(
   "docker:/Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion"
   "gem:$HOME/.usr/plugins/zsh/zsh-completions/src/_gem"
   "ghc:$HOME/.usr/plugins/zsh/zsh-completions/src/_ghc"
+  "git:/usr/local/share/zsh/site-functions/_git"
   "go:$HOME/.usr/plugins/zsh/zsh-completions/src/_go"
   "i:$HOME/Documents/src/projects/i/zsh-completion/_i"
   "iwfmlogs:$HOME/work/iwfm-logs/zsh-completion/_iwfmlogs"
@@ -28,6 +29,11 @@ COMPLETION_DEFINITIONS=(
 
 ZSH_SITE_FUNCTIONS=$HOME/.zsh/site-functions
 mkdir -p "$ZSH_SITE_FUNCTIONS"
+
+cp \
+  /usr/local/share/zsh/site-functions/git-completion.bash \
+  /usr/local/share/zsh/site-functions/tig-completion.bash \
+  "$ZSH_SITE_FUNCTIONS"
 
 for COMPLETION_DEFINITION in "${COMPLETION_DEFINITIONS[@]}" ; do
   NAME="${COMPLETION_DEFINITION%%:*}"
